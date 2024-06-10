20ddd3af9736f25a767656f7526e1cae2939c008
alan01777 <camp.alan3@gmail.com>
2024-06-10 09:44:49 -0300

    Refactor DataInserter update_data method to compare and update rows

--- utils/DataInserter.py
+++ utils/DataInserter.py
@@ -7,13 +7,17 @@ from utils import DataLoader
 class DataInserter:
     def __init__(self):
         self.conn = GSheetsConnection("gsheets", type=GSheetsConnection)
-        self.df = DataLoader().load_data()
+        self.old_data = DataLoader().load_data()
         
     def insert_data(self, data):
         self.df = self.df.append(data, ignore_index=True)
         self.conn.update(data=self.df)
         
-    def update_data(self, data):
-        self.conn.update(data=data)
-        st.cache_data.clear()
-        st.rerun()
+    def update_data(self, new_data, old_data):
+        # compare all the rows in the new data with the old data
+        # if the row is not in the old data or if the row is in the old data but has different values, update the row
+        for index, row in new_data.iterrows():
+            if index not in old_data.index or not old_data.loc[index].equals(row):
+                old_data.loc[index] = row
+        self.conn.update(data=old_data)
+        st.rerun() 

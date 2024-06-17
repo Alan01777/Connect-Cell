006aa9fc2a186b23869dccfe1f85e04d8b32a3bb
alan01777 <camp.alan3@gmail.com>
2024-06-17 11:21:46 -0300

    Add logo to Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -2,9 +2,10 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-from utils import DataLoader, DateFilter, Formatting
+from utils import DataLoader, DateFilter, Formatting, Logo
 from settings import page_settings
 from streamlit_extras.stylable_container import stylable_container
+from streamlit_extras.app_logo import add_logo
 
 
 class Dashboard:
@@ -591,6 +592,7 @@ class Dashboard:
             )
 
     def main(self):
+        add_logo("logo.png", height=250)
         st.title("Dashboard Financeiro e operacional")
         st.write(
             """
